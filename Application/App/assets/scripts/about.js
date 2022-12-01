e0d5c583c4e26b63ee4d513076cb9ae57c3d97ee
kinire98 <93119213+kinire98@users.noreply.github.com>
2022-12-01 14:39:58 +0100

    added things in the about page

--- Application/App/assets/scripts/about.js
+++ Application/App/assets/scripts/about.js
@@ -1,6 +1,13 @@
 import { glowCards } from "./modules/glowCards.js"
+import { getYear } from "./modules/getYear.js"
+import { cardText } from "./modules/cardText.js"
+import { openLink } from "./modules/openLink.js";
+import env from "../../helpers/env.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     glowCards();
+    getYear();
+    cardText();
+    openLink(env.github_page, ".card-icon");
 })
