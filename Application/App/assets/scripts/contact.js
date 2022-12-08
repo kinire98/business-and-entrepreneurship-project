0b71a72e684f2a8804daec8fba41510cf6f6171b
kinire98 <93119213+kinire98@users.noreply.github.com>
2022-12-08 19:13:50 +0100

    halo new stuff added I'm not sure a lot especially in the about page a lot of work to do still

--- Application/App/assets/scripts/contact.js
+++ Application/App/assets/scripts/contact.js
@@ -1,5 +1,7 @@
 import { cardText } from "./modules/cardText.js"
+import { getYear } from "./modules/getYear.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     cardText();
+    getYear();
 })
