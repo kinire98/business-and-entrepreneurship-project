31bb50eaf744bc916b447192c4318b8366d314b4
kinire98 <93119213+kinire98@users.noreply.github.com>
2023-01-04 19:43:45 +0100

    Navbar finished

--- Application/App/assets/scripts/modules/email.js
+++ Application/App/assets/scripts/modules/email.js
@@ -3,7 +3,8 @@ import { ajaxHelper } from "../../../helpers/ajaxHelper.js";
 export function email(eMail) {
 
     const $loader = document.querySelector("#email-form img"),
-        $submit = document.querySelector('#email-form input[type="submit"]');
+        $submit = document.querySelector('#email-form input[type="submit"]'),
+        $formElements = document.querySelectorAll("input, textarea, select");
     
 
 
@@ -14,6 +15,7 @@ export function email(eMail) {
         e.target.querySelector('[type="hidden"]').value = "Reply to " + e.target.querySelector('[type="hidden"]').value + ":";
         $loader.style.display = "block";
         $submit.style.display = "none";
+        $formElements.forEach(el => el.classList.add("blank"));
         ajaxHelper({
             URL: eMail,
             method: "POST",
@@ -23,13 +25,14 @@ export function email(eMail) {
             },
             body: new FormData(e.target),
             success: (json) => {
-                console.log(json)
                 document.querySelector("legend").innerHTML = "Thank you for you submission!"
                 $loader.style.display = "none";
+                $formElements.forEach(el => el.classList.remove("blank"));
             },
             error: (err) => {
                 document.querySelector("legend").innerHTML = "Sorry! There was an error"
                 $loader.style.display = "none";
+                $formElements.forEach(el => el.classList.remove("blank"));
             }
         })
     })
