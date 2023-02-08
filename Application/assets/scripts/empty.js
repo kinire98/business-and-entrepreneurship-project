d22e62cd7878e077e7fce0df5aea0cc2c6417c35
kinire98 <93119213+kinire98@users.noreply.github.com>
2023-02-08 20:15:56 +0100

    compromise ssr

--- Application/assets/scripts/empty.js
+++ Application/assets/scripts/empty.js
@@ -1,9 +1,12 @@
+import { appear, hamburger } from "./header.js";
 import { getYear } from "./modules/getYear.js";
 import { reveals } from "./modules/reveals.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     getYear(); 
     reveals();
+    appear();
+    hamburger();
 })
 window.addEventListener("scroll", reveals)
 
