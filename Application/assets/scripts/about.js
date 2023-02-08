41aa4df9db82612d19ffffa1cd4a3c10b567db6f
kinire98 <93119213+kinire98@users.noreply.github.com>
2023-02-08 21:18:18 +0100

    about and gols ssr

--- Application/assets/scripts/about.js
+++ Application/assets/scripts/about.js
@@ -1,14 +1,12 @@
+import { appear, hamburger } from "./header.js";
 import { getYear } from "./modules/getYear.js"
-import { cardText } from "./modules/cardText.js"
-import { openLink } from "./modules/openLink.js";
 import { reveals } from "./modules/reveals.js"
-import env from "../../helpers/env.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
     getYear();
-    cardText();
-    openLink(env.github_page, ".card-icon");
     reveals();
+    appear();
+    hamburger();
 })
 window.addEventListener("scroll", reveals)
--- Application/assets/scripts/gold.js
+++ Application/assets/scripts/gold.js
@@ -1,8 +1,11 @@
+import { appear, hamburger } from "./header.js";
 import { getYear } from "./modules/getYear.js"
 import { reveals } from "./modules/reveals.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     getYear();
     reveals();
+    appear();
+    hamburger();
 })
 window.addEventListener("scroll", reveals)
