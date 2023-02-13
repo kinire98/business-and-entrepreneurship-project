49e122481a212346c593bc5c946ab72140bae2c5
kinire98 <93119213+kinire98@users.noreply.github.com>
2023-02-13 18:31:26 +0100

    some adjusments to the css

--- Application/assets/scripts/about.js
+++ Application/assets/scripts/about.js
@@ -1,5 +1,6 @@
 import { appear, hamburger } from "./header.js";
 import { getYear } from "./modules/getYear.js"
+import { positionRemoveClass } from "./modules/positionRemoveClass.js";
 import { reveals } from "./modules/reveals.js"
 
 
@@ -8,5 +9,21 @@ document.addEventListener("DOMContentLoaded", () => {
     reveals();
     appear();
     hamburger();
-})
-window.addEventListener("scroll", reveals)
+    positionRemoveClass(document.getElementById("points"), 1000, 0);
+    positionRemoveClass(document.getElementById("back"), 1000, 0);
+    positionRemoveClass(document.querySelector("#values > .dots"), Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+                       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ), 1500);
+    positionRemoveClass(document.querySelector("#values > .back"), Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+                       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ), 1500);
+});
+window.addEventListener("scroll", () => {
+    reveals()
+    positionRemoveClass(document.getElementById("points"), 1000, 0);
+    positionRemoveClass(document.getElementById("back"), 1000, 0);
+    positionRemoveClass(document.querySelector("#values > .dots"), Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+                       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ), 1500);
+    positionRemoveClass(document.querySelector("#values > .back"), Math.max( document.body.scrollHeight, document.body.offsetHeight, 
+                       document.documentElement.clientHeight, document.documentElement.scrollHeight, document.documentElement.offsetHeight ), 1500);
+
+
+});
